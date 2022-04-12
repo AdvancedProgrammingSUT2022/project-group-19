641eb263f2d6a79877a825cc1ddddb50fd1a4f0e
satayyeb <salitayyeb@gmail.com>
2022-04-12 15:01:49 +0430

    some changes in .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello Sky Stars! This is a empty project.");
+        System.out.println("Hello Sky Stars! This is an empty project.");
         System.out.println("please configure your java version to 17");
+        System.out.println("good luck :)");
     }
 }
