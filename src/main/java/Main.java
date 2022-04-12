7cf3bec6199f064849d701676f7e0f410e81bb95
satayyeb <salitayyeb@gmail.com>
2022-04-13 01:12:16 +0430

    add land class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello Sky Stars! This is an empty project.");
-        System.out.println("please configure your java version to 17");
-        System.out.println("good luck :)");
+    public static void main(String[] args) {
+        System.out.println("""
+            Hello Sky Stars! This is an empty project.
+            please configure your java version to 17
+            good luck :)
+            """);
     }
 }
---
+++ src/main/java/model/land/Land.java
@@ -0,0 +1,15 @@
+package model.land;
+
+public abstract class Land {
+    private int food;
+    private int production;
+    private int gold;
+    private int movePoint;
+
+    public Land(int food, int production, int gold, int movePoint) {
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.movePoint = movePoint;
+    }
+}
