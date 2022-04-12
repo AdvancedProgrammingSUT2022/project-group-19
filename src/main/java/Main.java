a652df5f377df5fe3d200be2a7df88052587bd5f
satayyeb <salitayyeb@gmail.com>
2022-04-12 14:56:32 +0430

    add Main.java file

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello Sky Stars! This is a empty project.");
+        System.out.println("please configure your java version to 17");
+    }
+}
