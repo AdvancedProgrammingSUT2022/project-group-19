2e1f978a3611463248b93c345de73ac000ad8395
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-04-16 14:10:11 +0430

    add "possible resources and features" to landType class

--- src/main/java/model/land/LandType.java
+++ src/main/java/model/land/LandType.java
@@ -1,27 +1,68 @@
 package model.land;
 
-public enum LandType {
-    //TODO: Implement resources that may be found on each terrain and feature:
-    //TODO: Implement features that may be found on each terrain:
-    //terrains:
-    DESERT(0, 0, 0, -0.33, 1),
-    GRASS_LAND(2, 0, 0, -0.33, 1),
-    HILL(0, 2, 0, 0.25, 2),
-    MOUNTAIN(0, 0, 0, 0, 9999),
-    OCEAN(0, 0, 0, 0, 9999),
-    PLAIN(1, 1, 0, -0.33, 1),
-    SNOW(0, 0, 0, -0.33, 1),
-    TUNDRA(1, 0, 0, -0.33, 1),
+import model.resource.ResourceType;
 
+public enum LandType {
     //features:
-    JOLGE(2, 0, 0, -0.33, 1),
-    JUNGLE(1, 1, 0, 0.25, 2),
-    ICE(0, 0, 0, 0, 9999),
-    DARK_JUNGLE(1, -1, 0, 0.25, 2),
-    SWAMP(-1, 0, 0, -0.33, 2),
-    OASIS(3, 0, 1, -0.33, 1),
-    RIVER(0, 0, 1, 0, -1);
-    //TODO: Implement a mechanism that spoils all movePoints. for example: in RIVER, MP = -1 .
+    JOLGE(2, 0, 0, -0.33, 1,null,
+        new ResourceType[]{ResourceType.WHEAT,ResourceType.SUGAR}),
+
+    JUNGLE(1, 1, 0, 0.25, 2,null,
+        new ResourceType[]{ResourceType.GAZELLE,ResourceType.COLOR,ResourceType.SILK}),
+
+    ICE(0, 0, 0, 0, 9999,null,
+        new ResourceType[]{}),
+
+    DARK_JUNGLE(1, -1, 0, 0.25, 2,null,
+        new ResourceType[]{ResourceType.BANANA,ResourceType.COLOR}),
+
+    SWAMP(-1, 0, 0, -0.33, 2,null,
+        new ResourceType[]{ResourceType.SUGAR}),
+
+    OASIS(3, 0, 1, -0.33, 1,null,
+        new ResourceType[]{}),
+
+    RIVER(0, 0, 1, 0, 0,null,
+        new ResourceType[]{}),
+
+
+    //terrains:
+    DESERT(0, 0, 0, -0.33, 1,
+        new LandType[]{OASIS, JOLGE},
+        new ResourceType[]{ResourceType.IRON, ResourceType.GOLD, ResourceType.SILVER, ResourceType.MARBLE,
+            ResourceType.COTTON, ResourceType.INCENSE, ResourceType.SHEEP}),
+
+    GRASS_LAND(2, 0, 0, -0.33, 1,
+        new LandType[]{JUNGLE, SWAMP},
+        new ResourceType[]{ResourceType.IRON, ResourceType.HORSE, ResourceType.COAL, ResourceType.COW,
+            ResourceType.GOLD, ResourceType.MARBLE, ResourceType.COTTON, ResourceType.SHEEP}),
+
+    HILL(0, 2, 0, 0.25, 2,
+        new LandType[]{JUNGLE, DARK_JUNGLE},
+        new ResourceType[]{ResourceType.IRON, ResourceType.COAL, ResourceType.GAZELLE, ResourceType.GOLD,
+            ResourceType.SILVER, ResourceType.SHEEP}),
+
+    MOUNTAIN(0, 0, 0, 0, 9999,
+        new LandType[]{},
+        new ResourceType[]{}),
+
+    OCEAN(0, 0, 0, 0, 9999,
+        new LandType[]{},
+        new ResourceType[]{}),
+
+    PLAIN(1, 1, 0, -0.33, 1,
+        new LandType[]{JUNGLE, DARK_JUNGLE},
+        new ResourceType[]{ResourceType.IRON, ResourceType.HORSE, ResourceType.COAL, ResourceType.WHEAT,ResourceType.GOLD,
+            ResourceType.MARBLE, ResourceType.IVORY, ResourceType.COTTON, ResourceType.INCENSE, ResourceType.SHEEP}),
+
+    SNOW(0, 0, 0, -0.33, 1,
+        new LandType[]{},
+        new ResourceType[]{ResourceType.IRON}),
+
+    TUNDRA(1, 0, 0, -0.33, 1,
+        new LandType[]{JUNGLE},
+        new ResourceType[]{ResourceType.IRON,ResourceType.HORSE,ResourceType.GAZELLE,ResourceType.SILVER, ResourceType.MARBLE});
+
 
     private final int food;
     private final int gold;
@@ -29,7 +70,7 @@ public enum LandType {
     private final int production;
     private final double fightChanges;
 
-    LandType(int food, int production, int gold, double fightChanges, int movePoint) {
+    LandType(int food, int production, int gold, double fightChanges, int movePoint, LandType[] possibleFeatures, ResourceType[] possibleResources) {
         this.food = food;
         this.gold = gold;
         this.movePoint = movePoint;
