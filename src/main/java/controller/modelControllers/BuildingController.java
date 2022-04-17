a224487ceffac2a180aa808a1027a9becfad4fd8
SajjadZr <94253836+SajjadZr@users.noreply.github.com>
2022-04-18 02:37:55 +0430

    update Building Controller!

--- src/main/java/controller/modelControllers/BuildingController.java
+++ src/main/java/controller/modelControllers/BuildingController.java
@@ -1,19 +1,62 @@
 package controller.modelControllers;
 
+import model.building.Building;
+import model.civilizations.City;
 import model.civilizations.Civilization;
 import model.technology.Technology;
 
+import java.util.ArrayList;
+
 public class BuildingController {
-    Civilization civilization;
+    private ArrayList<Civilization> civilizations =  new ArrayList<>();
 
-    public BuildingController(){
-        this.civilization = new Civilization();
-    }
 
-    public boolean isValidTechnology(Technology technology){
+
+    public boolean isValidTechnology(Civilization civilization, Technology technology){
         return civilization.getReachedTechs().contains(technology);
     }
 
+    public void buildCastle(Civilization civilization){
 
+    }
 
+    public void DestroyingMilitaryBuildings(City city){
+
+    }
+
+    public void randomDestroyingOtherBuildings(City city){
+
+    }
+
+    public int castleGoldProduction(Civilization civilization){
+
+    }
+
+    public int castleScienceProduction(Civilization civilization){
+
+    }
+
+    public int castleProduction(Civilization civilization){
+
+    }
+
+    public void payBuildingMaintenance(Building building){
+
+    }
+
+    public void changeBuilding(City city){
+
+    }
+
+    public void CreateAnExpert(Building building){
+
+    }
+
+    public int growthRateCity(){
+
+    }
+
+    public void buildBuilding(){
+
+    }
 }
