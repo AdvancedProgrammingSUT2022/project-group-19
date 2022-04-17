aee3c897ad491b031a8032c656f2a49c814d8bf5
SajjadZr <94253836+SajjadZr@users.noreply.github.com>
2022-04-18 01:40:07 +0430

    add gameMenuController + update City

--- src/main/java/controller/modelControllers/BuildingController.java
+++ src/main/java/controller/modelControllers/BuildingController.java
@@ -14,4 +14,6 @@ public class BuildingController {
         return civilization.getReachedTechs().contains(technology);
     }
 
+
+
 }
--- src/main/java/model/civilizations/City.java
+++ src/main/java/model/civilizations/City.java
@@ -1,7 +1,18 @@
 package model.civilizations;
 
+import model.building.Building;
+import model.game.Tile;
+import model.unit.Unit;
+
+import java.util.ArrayList;
+
 public class City {
     private String name;
     private int defensivePower;
-
+    private boolean isCapital;
+    private int food;
+    private int production;
+    private ArrayList<Unit> units;
+    private ArrayList<Tile> tiles;
+    private ArrayList<Building> buildings;
 }
