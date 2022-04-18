e73a3e34c2b533f5ffabdaf3bfb657117232bf99
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-04-18 14:54:25 +0430

    move some methods to Unit.java and Implement "turn" and the "movePoint decrease/increase"

--- src/main/java/model/civilizations/Civilization.java
+++ src/main/java/model/civilizations/Civilization.java
@@ -18,93 +18,19 @@ public class Civilization {
     private int population;
     private int cups;
 
-    public void resetMPofAllUnits(){}
 
-    public String getLeaderName() {
-        return leaderName;
+    /*
+     * This method must be called at the beginning of each turn.
+     */
+    public void resetMPofAllUnits() {
+        for (Unit unit : units)
+            unit.resetMP();
     }
 
-    public void setLeaderName(String leaderName) {
-        this.leaderName = leaderName;
-    }
 
-    public ArrayList<City> getCities() {
-        return cities;
-    }
-
-    public void setCities(ArrayList<City> cities) {
-        this.cities = cities;
-    }
 
     public ArrayList<Technology> getReachedTechs() {
         return reachedTechs;
     }
 
-    public void setReachedTechs(ArrayList<Technology> reachedTechs) {
-        this.reachedTechs = reachedTechs;
-    }
-
-    public ArrayList<Technology> getInStudyTechs() {
-        return InStudyTechs;
-    }
-
-    public void setInStudyTechs(ArrayList<Technology> inStudyTechs) {
-        InStudyTechs = inStudyTechs;
-    }
-
-    public int getGold() {
-        return Gold;
-    }
-
-    public void setGold(int gold) {
-        Gold = gold;
-    }
-
-    public int getProduction() {
-        return production;
-    }
-
-    public void setProduction(int production) {
-        this.production = production;
-    }
-
-    public int getHappinessIndex() {
-        return happinessIndex;
-    }
-
-    public void setHappinessIndex(int happinessIndex) {
-        this.happinessIndex = happinessIndex;
-    }
-
-    public int getScienceIndex() {
-        return scienceIndex;
-    }
-
-    public void setScienceIndex(int scienceIndex) {
-        this.scienceIndex = scienceIndex;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
-
-    public int getCups() {
-        return cups;
-    }
-
-    public void setCups(int cups) {
-        this.cups = cups;
-    }
-
-    public ArrayList<Unit> getUnits() {
-        return units;
-    }
-
-    public void setUnits(ArrayList<Unit> units) {
-        this.units = units;
-    }
 }
