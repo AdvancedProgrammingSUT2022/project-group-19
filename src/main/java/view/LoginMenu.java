a8f1deeaecded2be0b67dc62d361682ca6c0e65f
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-04-24 02:18:59 +0430

    full edit of the view's structure

--- src/main/java/view/LoginMenu.java
+++ src/main/java/view/LoginMenu.java
@@ -2,7 +2,6 @@ package view;
 
 import model.Database;
 import model.Function;
-import model.User;
 
 import java.util.HashMap;
 import java.util.Scanner;
@@ -10,28 +9,44 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginMenu {
-    protected Database database;
-    protected Matcher matcher;
-    protected String command;
+    protected Database database = new Database();
     protected Scanner scanner = new Scanner(System.in);
-
-    private final HashMap<String, Function> functions = new HashMap<>() {{
+    protected Matcher matcher;
+    private final HashMap<String, Function> functions = new HashMap<>();
+    protected HashMap<String, Function> basicFunctions = new HashMap<>() {{
         put("^menu exit$", null);
         put("^menu show-current$", () -> showCurrentMenu());
         put("^menu enter (?<menuName>(login menu|main menu|profile menu|game menu))$", () -> gotoMenu());
-        put("^user login --username (?<username>.+) --password (?<password>.+)$", () -> login());
-        put("^user create --username (?<username>.+) --nickname (?<nickname>.+) --password (?<password>.+)$", () -> addUser());
     }};
 
+    public LoginMenu() {
+        functions.putAll(basicFunctions);
+        functions.put("^user login --username (?<username>.+) --password (?<password>.+)$", this::login);
+        functions.put("^user create --username (?<username>.+) --nickname (?<nickname>.+) --password (?<password>.+)$", this::addUser);
+    }
+
     public void run() {
+        menuLoop(functions);
+    }
+
+    protected void gotoMenu() {
+        String nextMenuName = matcher.group("menuName");
+        if (nextMenuName.equals("main menu")) {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.run();
+        } else
+            System.out.println("menu navigation is not possible");
+    }
+
+    protected void menuLoop(HashMap<String, Function> functions) {
         while (true) {
-            command = scanner.nextLine();
+            String command = scanner.nextLine();
             boolean validCommand = false;
             for (String regex : functions.keySet()) {
                 matcher = Pattern.compile(regex).matcher(command);
                 if (matcher.find()) {
                     if (functions.get(regex) == null)
-                        return; //exit the menu
+                        return;
                     functions.get(regex).function();
                     validCommand = true;
                     break;
@@ -47,11 +62,8 @@ public class LoginMenu {
         String password = matcher.group("password");
         String nickname = matcher.group("nickname");
         //TODO this
-        String message = "test";// = Controller.checkNewUser(username, password, nickname);
-        if (message.equals("it is ok")) {
-            User user = new User(username, password, nickname);
-            //database.addUser(user);
-        }
+        String message = "test";//= Controller.checkNewUser(username, password, nickname);
+        System.out.println(message);
     }
 
     private void login() {
@@ -60,31 +72,10 @@ public class LoginMenu {
         //TODO this
         String message = "test";// = Controller.loginCheck(username,password);
         System.out.println(message);
-        if (message.equals("user logged in successfully!")) {
-            //TODO: go to main menu
-        }
     }
 
-    private void showCurrentMenu() {
+    protected void showCurrentMenu() {
         String className = this.getClass().getSimpleName();
         System.out.println(className);
-    }
-
-    protected void gotoMenu() {
-        String nextMenuName = matcher.group("menuName");
-        String currentMenuName = this.getClass().getSimpleName();
-        switch (nextMenuName) {
-            case "login menu":
-                break;
-            //TODO: in anjam beshe
-            case "main menu":
-                break;
-
-            case "profile menu":
-                break;
-
-            case "game menu":
-                break;
-        }
     }
 }
